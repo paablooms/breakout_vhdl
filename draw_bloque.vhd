f6667f2af0aaaa0fc48b4467f002bca1e6491513
paablooms <pamarsierra@gmail.com>
2025-12-03 12:19:52 +0100

    Update draw_bloque.vhd

--- draw_bloque.vhd
+++ draw_bloque.vhd
@@ -33,10 +33,10 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity draw_bloque is
   Port (
-    data_out_B: in std_logic;  --bit leído de la RAM que te dice si hay bloque ahí o no
-    ejex: in std_logic_vector(9 downto 0); --coordenada x del píxel actual
-    ejey: in std_logic_vector(9 downto 0); --coordenada y del píxel actual
-    ADDR_B: out std_logic_vector(7 downto 0); --dirección en memoria
+    data_out_B: in std_logic;  --bit leÃ­do de la RAM que te dice si hay bloque ahÃ­ o no
+    ejex: in std_logic_vector(9 downto 0); --coordenada x del pÃ­xel actual
+    ejey: in std_logic_vector(9 downto 0); --coordenada y del pÃ­xel actual
+    ADDR_B: out std_logic_vector(7 downto 0); --direcciÃ³n en memoria
     RGB_bloque: out std_logic_vector(11 downto 0)); --ccolor del bloque
 end draw_bloque;
 
@@ -51,7 +51,7 @@ begin
     ADDR_B(3 downto 0) <= ejex(8 downto 5); --32 de ancho
     ADDR_B(7 downto 4) <= ejey(7 downto 4); --16 de alto
     
-    --  ¿hay bloque? ¿está el bloque en la zona específica para los bloques?
+    --  Â¿hay bloque? Â¿estÃ¡ el bloque en la zona especÃ­fica para los bloques?
     if (data_out_B = '1') and  (unsigned(ejex) < to_unsigned(512, 10)) and
    (unsigned(ejey) < to_unsigned(256, 10)) then
         RGB_bloque <= "000000001111"; --dibuja el bloque
