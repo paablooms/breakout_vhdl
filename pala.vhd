c540d7f834cce6f8c31d9e44a93eedcd4a9907d9
paablooms <pamarsierra@gmail.com>
2025-12-03 15:25:08 +0100

    Update pala.vhd

--- pala.vhd
+++ pala.vhd
@@ -52,22 +52,40 @@ begin
 mov: process(clk, reset)
 begin
     if reset = '1' then
-        posp <= to_unsigned(255, 10);  -- posición inicial
+        posp <= to_unsigned(223, 10);  -- posición inicial
     elsif rising_edge(clk) then
-        if refresh = '1' then             -- solo muevo cuando refresco
-            if (left = '1' and posp > 32) then
+    -- valor por defecto para evitar latches
+    posp <= posp;
+
+    if refresh = '1' then
+        if left = '1' then
+            if posp > 0 then
                 posp <= posp - 1;
-            elsif (right = '1' and posp < 480) then
-                posp <= posp + 1;
+            else
+                posp <= posp;  -- alcanza límite inferior
             end if;
+
+        elsif right = '1' then
+            if posp < 480 then
+                posp <= posp + 1;
+            else
+                posp <= posp;  -- alcanza límite superior
+            end if;
+
+        else
+            posp <= posp;      -- ni left ni right
         end if;
+
+    else
+        posp <= posp;          -- sin refresh
     end if;
+end if;
 end process;
 
 
 rgb: process(ejex,ejey,posp)
 	begin
-		if(unsigned(ejex) > posp and unsigned(ejex)<posp+80 and unsigned(ejey)>10 and unsigned(ejey)<30) then
+		if(unsigned(ejex) > posp and unsigned(ejex)<posp+64 and unsigned(ejey)>223 and unsigned(ejey)<239) then
 			RGBpala <= "111100000000";
     else
 			RGBpala <= (others => '0');
